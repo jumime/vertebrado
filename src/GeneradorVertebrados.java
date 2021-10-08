7a61031dad4809856c0be3049ccaf3eec7ec6032
jumime <granadosmj@uninorte.edu.co>
2021-10-08 17:54:43 -0500

    Coloque ternero en el indice 1

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,7 +2,7 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[1] = new Vertebrado("Ternero", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburon", "Pez");
 
         return miVertebrados;
